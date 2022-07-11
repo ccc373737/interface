4c5165f6414002a6b9ede345904239964ff37417
ccc <chao373737@gmail.com>
2022-07-11 15:07:45 +0800

    111

--- src/data/Allowances.ts
+++ src/data/Allowances.ts
@@ -6,7 +6,7 @@ import { useSingleCallResult } from '../state/multicall/hooks'
 
 export function useTokenAllowance(token?: Token, owner?: string, spender?: string): TokenAmount | undefined {
   const contract = useTokenContract(token?.address, false)
-
+  //
   const inputs = useMemo(() => [owner, spender], [owner, spender])
   const allowance = useSingleCallResult(contract, 'allowance', inputs).result
 
